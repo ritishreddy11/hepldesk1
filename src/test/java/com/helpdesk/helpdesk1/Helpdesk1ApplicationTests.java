9ef6c21f3848f96115f6c638555bb9de2ee66946
Ritish Reddy <ritisreddy1@gmail.com>
2024-02-20 23:38:37 +0530

    start project

---
+++ src/test/java/com/helpdesk/helpdesk1/Helpdesk1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helpdesk.helpdesk1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Helpdesk1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
